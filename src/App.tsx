623dce309f8e5880644b1cca38ba350de843f9c2
Felipe <feferreira.sf@gmail.com>
2023-03-29 19:07:04 -0300

    refactor: app reconfigurator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { Header } from "@components/Header";
 import { GlobalStyle } from "@styles/global";
 import { defaultTheme } from "@styles/theme/default";
+import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { DefaulLayout } from "./layout/DefaultLayout";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <DefaulLayout />
+      <Header />
+      <Outlet />
     </ThemeProvider>
   );
 }
