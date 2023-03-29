a649d55d3133b1ad0fffa8718eeb3967dd5f0d6b
Felipe <feferreira.sf@gmail.com>
2023-03-29 16:58:22 -0300

    styles: change line-height

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,14 +16,19 @@ export const GlobalStyle = createGlobalStyle`
     box-shadow: 0 0 0 2px ${({ theme }) => theme.colors["yellow-dark"]};
 
   }
+  button {
+    line-height: 0;
+    border: 0;
+    background: transparent;
+  }
   body, button,input, textarea {
     font-family: ${({ theme }) => theme.fontFamilies["main-sans"]};
     font-size: ${({ theme }) => theme.fontSizes.base};
     font-weight: ${({ theme }) => theme.fontWeights.regular};
-    line-height: 130%;
   }
   body {
     color: ${({ theme }) => theme.colors["base-text"]};
+    line-height: 130%;
     background: ${({ theme }) => theme.colors.background}
   }
 `;
