e31b5764bb734bb5bb25f204c48e0c7c8107eb81
Felipe <feferreira.sf@gmail.com>
2023-03-29 16:59:00 -0300

    feat: imported default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { GlobalStyle } from "@styles/global";
 import { defaultTheme } from "@styles/theme/default";
 import { ThemeProvider } from "styled-components";
+import { DefaulLayout } from "./layout/DefaultLayout";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <DefaulLayout />
     </ThemeProvider>
   );
 }
