bd2789e905d0bce0b91ea321d9be5a0873c35b57
Felipe <feferreira.sf@gmail.com>
2023-03-31 15:30:21 -0300

    feat: adding products components to home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "./components/Hero";
+import { Products } from "./components/Products";
 import { HomeContainer } from "./style";
 
 export const Home = () => {
   return (
     <HomeContainer>
       <Hero />
+      <Products />
     </HomeContainer>
   );
 };
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,16 +1,4 @@
 import styled from "styled-components";
 
 export const HomeContainer = styled.main`
-  /* max-width: 90rem; */
-  /* width: 100%; */
-  /* margin: 0 auto; */
-  padding: 0 .5rem;
-
-  @media ${({ theme }) => theme.breakpoints.mobileL} {
-    padding: 0rem 2rem;
-  }
-
-  @media ${({ theme }) => theme.breakpoints.desktop} {
-    padding: 0rem 10rem;
-  }
 `;
