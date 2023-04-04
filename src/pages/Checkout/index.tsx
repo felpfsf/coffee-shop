6e2e2856086c4efc3e496ebc996427ce7adf0588
Felipe <feferreira.sf@gmail.com>
2023-04-04 20:15:03 -0300

    feat: checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,7 +1,137 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Minus,
+  Money,
+  Plus,
+  Trash,
+} from "@phosphor-icons/react";
+import {
+  CartContainer,
+  CartProduct,
+  CartSummary,
+  CheckoutContainer,
+  CheckoutForm,
+  Counter,
+  FormContainer,
+  FormContainerHeader,
+  PaymentOptions,
+  ProductActions,
+  ProductInfo,
+  RadioButtonGroup,
+  RadioButtonItem,
+  RemoveButton,
+  SubmitButton,
+  Title,
+} from "./style";
+
 export const Checkout = () => {
   return (
-    <div>
-      <h1>Checkout</h1>
-    </div>
+    <CheckoutContainer>
+      <div>
+        <Title>Complete seu pedido</Title>
+        <FormContainer>
+          <FormContainerHeader>
+            <MapPinLine size={22} />
+            <div>
+              <h1>Endereço de entrega</h1>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </FormContainerHeader>
+          <CheckoutForm id='checkout_form'>
+            <input type='text' placeholder='CEP' />
+            <input type='text' placeholder='Rua' />
+            <div>
+              <input type='text' placeholder='Número' />
+              <input type='text' placeholder='Complemento' />
+            </div>
+            <div>
+              <input type='text' placeholder='Bairro' />
+              <input type='text' placeholder='Cidade' />
+              <input type='text' placeholder='UF' />
+            </div>
+          </CheckoutForm>
+        </FormContainer>
+        <PaymentOptions>
+          <FormContainerHeader>
+            <CurrencyDollar size={22} />
+            <div>
+              <h1>Pagamento</h1>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </FormContainerHeader>
+          <RadioButtonGroup>
+            <RadioButtonItem value='credit'>
+              <CreditCard size={16} />
+              <span>cartão de crédito</span>
+            </RadioButtonItem>
+            <RadioButtonItem value='debit'>
+              <Bank size={16} />
+              <span>cartão de débito</span>
+            </RadioButtonItem>
+            <RadioButtonItem value='cash'>
+              <Money size={16} />
+              <span>dinheiro</span>
+            </RadioButtonItem>
+          </RadioButtonGroup>
+        </PaymentOptions>
+      </div>
+      <div>
+        <Title>Café selecionados</Title>
+        <CartContainer>
+          <CartProduct>
+            <ProductInfo>
+              <img src='/products/expresso.png' alt='' />
+              <div>
+                <p>Expresso Tradicional</p>
+                <ProductActions>
+                  <Counter>
+                    <button>
+                      <Minus />
+                    </button>
+                    <span>1</span>
+                    <button>
+                      <Plus />
+                    </button>
+                  </Counter>
+                  <RemoveButton>
+                    <Trash size={16} />
+                    Remover
+                  </RemoveButton>
+                </ProductActions>
+              </div>
+            </ProductInfo>
+            <p>Price</p>
+          </CartProduct>
+          <CartSummary>
+            <div>
+              <p>Total de itens</p>
+              <p>
+                <span>R$ </span>29,70
+              </p>
+            </div>
+            <div>
+              <p>Entrega</p>
+              <p>
+                <span>R$ </span>3,50
+              </p>
+            </div>
+            <div>
+              <p>Total</p>
+              <p>
+                <span>R$ </span>33,20
+              </p>
+            </div>
+          </CartSummary>
+          <SubmitButton type='submit' form='checkout_form'>
+            Confirmar Pedido
+          </SubmitButton>
+        </CartContainer>
+      </div>
+    </CheckoutContainer>
   );
 };
---
+++ src/pages/Checkout/style.ts
@@ -0,0 +1,237 @@
+import styled from "styled-components";
+import * as RadioGroup from "@radix-ui/react-radio-group";
+
+export const CheckoutContainer = styled.main`
+  max-width: 90rem;
+  width: 100%;
+  margin: 2rem auto;
+  padding: 0 1rem;
+  @media ${({ theme }) => theme.breakpoints.mobileL} {
+    padding: 0rem 2rem;
+  }
+  @media ${({ theme }) => theme.breakpoints.desktop} {
+    padding: 0rem 10rem;
+    display: grid;
+    grid-template-columns: 40rem 1fr;
+    column-gap: 2rem;
+  }
+`;
+export const Title = styled.h1`
+  font-size: ${({ theme }) => theme.fontSizes.medium};
+  font-family: ${({ theme }) => theme.fontFamilies.title};
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  color: ${({ theme }) => theme.colors["base-subtitle"]};
+`;
+
+export const FormContainer = styled.div`
+  margin-top: 0.75rem;
+  padding: 2.5rem;
+  background: ${({ theme }) => theme.colors["base-card"]};
+  border-radius: 0.375rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  svg {
+    color: ${({ theme }) => theme.colors["yellow-dark"]};
+  }
+`;
+
+export const FormContainerHeader = styled.header`
+  display: flex;
+  gap: 0.5rem;
+  h1 {
+    font-size: ${({ theme }) => theme.fontSizes.base};
+    font-weight: ${({ theme }) => theme.fontWeights.regular};
+  }
+  p {
+    font-size: ${({ theme }) => theme.fontSizes.small};
+  }
+`;
+
+export const CheckoutForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  input {
+    background: ${({ theme }) => theme.colors["base-input"]};
+    border: 0.0625rem solid ${({ theme }) => theme.colors["base-button"]};
+    border-radius: 0.25rem;
+    padding: 0.75rem;
+  }
+  div {
+    display: flex;
+    gap: 0.75rem;
+    input {
+      flex: 1;
+      width: 0;
+    }
+  }
+  div:first-of-type {
+    input:last-of-type {
+      flex: 2.5;
+    }
+  }
+  div:last-of-type {
+    input:nth-of-type(1) {
+      flex: 1.1;
+    }
+    input:nth-of-type(2) {
+      flex: 2;
+      width: 0;
+    }
+    input:nth-of-type(3) {
+      flex: 0.1;
+      width: 3.75rem;
+    }
+  }
+`;
+
+export const PaymentOptions = styled.div`
+  margin-top: 0.75rem;
+  background: ${({ theme }) => theme.colors["base-card"]};
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  svg {
+    color: ${({ theme }) => theme.colors.purple};
+  }
+`;
+
+export const RadioButtonGroup = styled(RadioGroup.Root)`
+  display: flex;
+  gap: 0.75rem;
+`;
+
+export const RadioButtonItem = styled(RadioGroup.Item)`
+  width: 100%;
+  padding: 1rem;
+  border-radius: 0.375rem;
+  border: 1px solid transparent;
+  background: ${({ theme }) => theme.colors["base-button"]};
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  cursor: pointer;
+  span {
+    font-size: ${({ theme }) => theme.fontSizes.tiny};
+    text-transform: uppercase;
+  }
+  &:hover {
+    background: ${({ theme }) => theme.colors["base-hover"]};
+    transition: background 0.2s ease-in-out;
+  }
+  &[data-state="checked"] {
+    background: ${({ theme }) => theme.colors["purple-light"]};
+    border: 1px solid ${({ theme }) => theme.colors.purple};
+  }
+`;
+
+export const SubmitButton = styled.button`
+  font-size: ${({ theme }) => theme.fontSizes.small};
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  color: ${({ theme }) => theme.colors.white};
+  line-height: 160%;
+  text-transform: uppercase;
+  width: 100%;
+  padding: 12px 8px;
+  border-radius: 6px;
+  background: ${({ theme }) => theme.colors.yellow};
+  cursor: pointer;
+  &:focus {
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.yellow};
+  }
+  &:hover {
+    background: ${({ theme }) => theme.colors["yellow-dark"]};
+    transition: background 0.2s ease-in-out;
+  }
+`;
+
+export const CartContainer = styled.div`
+  margin-top: 0.75rem;
+  padding: 2.5rem;
+  background: ${({ theme }) => theme.colors["base-card"]};
+  border-radius: 0.375rem 2.75rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const CartProduct = styled.div`
+  display: flex;
+  justify-content: space-between;
+  img {
+    width: 64px;
+    height: 64px;
+  }
+`;
+
+export const ProductInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.25rem;
+`;
+
+export const ProductActions = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const Counter = styled.div`
+  height: 32px;
+  color: ${({ theme }) => theme.colors["base-title"]};
+  padding: 0.5rem;
+  background: ${({ theme }) => theme.colors["base-button"]};
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  button {
+    border-radius: 100%;
+    color: ${({ theme }) => theme.colors.purple};
+    cursor: pointer;
+  }
+  button:hover {
+    color: ${({ theme }) => theme.colors["purple-dark"]};
+    background: ${({ theme }) => theme.colors["base-hover"]};
+    transition: background 0.2s ease-in-out;
+  }
+`;
+
+export const RemoveButton = styled.button`
+  font-size: ${({ theme }) => theme.fontSizes.tiny};
+  text-transform: uppercase;
+  padding: 0.5rem;
+  border-radius: 0.375rem;
+  background: ${({ theme }) => theme.colors["base-button"]};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.25rem;
+  cursor: pointer;
+  svg {
+    color: ${({ theme }) => theme.colors.purple};
+  }
+  &:hover {
+    background: ${({ theme }) => theme.colors["base-hover"]};
+    transition: background 0.2s ease-in-out;
+  }
+`;
+
+export const CartSummary = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  div:last-child {
+    font-size: ${({ theme }) => theme.fontSizes.large};
+    font-weight: ${({ theme }) => theme.fontWeights.bold};
+    color: ${({ theme }) => theme.colors["base-subtitle"]};
+  }
+`;
