995d947193bdd13f8f21ee8b97c35636fc9f4da4
Felipe <feferreira.sf@gmail.com>
2023-04-06 14:20:52 -0300

    feat: show total items in cart over cart button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
 import { ShoppingCart } from "@phosphor-icons/react";
 import { CartButton, Counter, HeaderContainer } from "./style";
+import { useCart } from "@contexts/CartContext";
 
 export const Header = () => {
+  const { cartItems } = useCart();
+  const itemsInCart = cartItems.length;
   return (
     <HeaderContainer>
       <img src='/Logo.png' alt='' />
       <div>
         <CartButton>
           <ShoppingCart size={22} weight='fill' />
-          <Counter>3</Counter>
+          {itemsInCart > 0 && <Counter>{itemsInCart}</Counter>}
         </CartButton>
       </div>
     </HeaderContainer>
