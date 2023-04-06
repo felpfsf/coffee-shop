56349302157ee540835429c59b0a3b599ffff3f4
Felipe <feferreira.sf@gmail.com>
2023-04-06 13:22:02 -0300

    refactor: reworking context

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Header } from "@components/Header";
-import { ProductsContextProvider } from "@contexts/ProductsContext";
 import { GlobalStyle } from "@styles/global";
 import { defaultTheme } from "@styles/theme/default";
 import { Outlet } from "react-router-dom";
@@ -8,11 +7,9 @@ import { ThemeProvider } from "styled-components";
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <ProductsContextProvider>
-        <GlobalStyle />
-        <Header />
-        <Outlet />
-      </ProductsContextProvider>
+      <GlobalStyle />
+      <Header />
+      <Outlet />
     </ThemeProvider>
   );
 }
--- src/contexts/useProductsContext.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from "react";
-import { ProductsContext, ProductsContextData } from "./ProductsContext";
-
-export const useProductsContext = (): ProductsContextData =>
-  useContext(ProductsContext);
