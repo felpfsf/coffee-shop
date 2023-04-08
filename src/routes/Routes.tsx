093e0b184bb76d317941024f80eb24788ddd6134
Felipe <feferreira.sf@gmail.com>
2023-04-08 19:33:22 -0300

    feat: success route and protection

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter, useNavigate } from "react-router-dom";
 import App from "../App";
-import { Checkout, Home } from "@pages/index";
+import { Checkout, Home, Success } from "@pages/index";
+import { ReactNode, useState } from "react";
+import { useCart } from "@contexts/CartContext";
+
+const ProtectedRoute = ({ element }: { element: ReactNode }) => {
+  const navigate = useNavigate();
+  const { isOrderSubmitted } = useCart();
+  // const [submitted, setSubmitted] = useState(false);
+  // const handleSubmit = async () => {
+  //   await new Promise((resolve) => setTimeout(resolve, 2000));
+  //   setSubmitted(true);
+  // };
+  // if (!submitted) {
+  //   navigate("/");
+  //   return null;
+  // }
+  // return element as JSX.Element;
+  console.log(isOrderSubmitted);
+  return isOrderSubmitted ? (
+    (element as JSX.Element)
+  ) : (
+    <Navigate to='/' replace={true} state={{ from: "/success" }} />
+  );
+};
 
 export const Routes = createBrowserRouter([
   {
@@ -14,6 +37,11 @@ export const Routes = createBrowserRouter([
       {
         path: "/checkout",
         element: <Checkout />,
+      },
+      {
+        path: "/success",
+        element: <ProtectedRoute element={<Success />} />,
+        // element: <Success />,
       },
     ],
   },
